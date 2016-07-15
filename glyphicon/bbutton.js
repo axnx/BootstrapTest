9380a181cb61ab3465c087b4c5be417424974ce7
ms748 <storost@web.de>
2016-07-15 10:44:51 +0200

    new

---
+++ glyphicon/bbutton.js
@@ -0,0 +1,20 @@
+var bsbutton = {
+	btn_default: "btn-default",
+	btn_primary: "btn-primary",
+	btn_success: "btn-success",
+	btn_info: "btn-info",
+	btn_danger: "btn-danger",
+	btn_link: "btn-link"
+}
+
+var = bsbuttonsize = {
+	btn_lg: "btn-lg",
+	btn_md: "btn-md",
+	btn_sm: "btn-sm",
+	btn_xs: "btn-xs"
+}
+
+var = bsbuttonactive = {
+	active: "active",
+	disabled: "disabled"
+}
